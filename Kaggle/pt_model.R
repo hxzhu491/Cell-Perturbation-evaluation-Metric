7c1e042ae7385ea84276fe8674d3c0426de9e376
hxzhu491 <zhuhongxuai3@gmail.com>
2024-09-18 12:35:42 -0500

    Small modification on pbmc code

--- Kaggle/pt_model.R
+++ Kaggle/pt_model.R
@@ -1,24 +1,22 @@
-fit <- lm(as.matrix(bulk_adata_X) ~ bulk_meta$sm_name, x=TRUE)
+fit <- lm(as.matrix(bulk_X_d0) ~ bulk_meta_d0$sm_name, x=TRUE)
 
-pt_pred <- list()
-for (i in 1: nrow(cmb_df)){
-  ix <- cmb_df$cell_type==cmb_df$cell_type[i] & cmb_df$sm_name==cmb_df$sm_name[i]
+pt_pred_d0 <- list()
+for (i in 1: 30){
+  ix <- bulk_meta_d0$cell_type==cmb_d0_df$cell_type[i] & bulk_meta_d0$sm_name==cmb_d0_df$sm_name[i]
   obs_test <- fit$x[ix,]
   obs_train <- fit$x[!ix,]
   
-  X_test <-as.matrix(bulk_adata_X[ix,]) 
-  X_train <- as.matrix(bulk_adata_X[!ix,])
+  X_test <- as.matrix(bulk_X_d0[ix,]) 
+  X_train <- as.matrix(bulk_X_d0[!ix,])
   
   fit.x <- lm.fit(obs_train,X_train)
-  pred_pt <- obs_test%*%fit.x$coefficients
-  names(pred_pt) <- gene_name
-  pt_pred[[i]] <- pred_pt[1,]
+  pred_pt_d0 <- obs_test%*%fit.x$coefficients
+  names(pred_pt_d0) <- gene_name
+  pt_pred_d0[[i]] <- pred_pt_d0[1,]
 }
 
-pt_pred_df <- data.frame()
-for (i in 1:length(pt_pred)){
-  pt_pred_df <- data.frame(rbind(pt_pred_df,as.vector(pt_pred[[i]])))
+pt_pred_d0_df <- data.frame()
+for (i in 1:length(pt_pred_d0)){
+  pt_pred_d0_df <- data.frame(rbind(pt_pred_d0_df,as.vector(pt_pred_d0[[i]])))
 } 
-names(pt_pred_df) <- gene_name
-
-write.csv(pt_pred_df, "./Rmd_output/pert_model_pred.csv")
+names(pt_pred_d0_df) <- gene_name
